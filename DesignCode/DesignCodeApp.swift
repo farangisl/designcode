98c7e654eed490394b7a32e907a3fa8a17dce6c2
Farangis <Farangis>
2023-03-06 15:22:00 +0500

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Махмадёрова Фарангис Шухратовна on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
